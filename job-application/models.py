8e82bbe503fc5af31a29dc8bd9a5cd84f3a1ff03
Chelsy Matuto <chelsymatuto21@gmail.com>
2026-03-31 15:32:41 +0800

    debug the dashboard. missing endblock

--- job-application/models.py
+++ job-application/models.py
@@ -393,3 +393,5 @@ class JobImage(db.Model):
     job_id = db.Column(db.Integer, db.ForeignKey("job.id"), nullable=False)
     image_path = db.Column(db.String(200))
     job = db.relationship("Job", backref="images")  
+    # =========================
+    # =========================
